9344a2a1f9241d722265b14591db23ca8c545f12
Jubaraj Mukherjee <59380734+jubarajmukherjee@users.noreply.github.com>
2021-07-26 22:54:44 +0530

    Add files via upload
    first release

---
+++ webscraper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+#url=input("Enter the desired url to scrape.")
+url="https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+r = requests.get(url)
+c = r.content
+
+soup = BeautifulSoup(c,"html.parser")
+#print(soup.prettify)
+
+name=[]
+price=[]
+rating=[]
+
+
+name1=[]
+price1=[]
+rating1=[]
+
+name=soup.find_all("div",{"class":"_4rR01T"})
+price=soup.find_all("div",{"class":"_30jeq3 _1_WHN1"})
+rating=soup.find_all("div",{"class":"_3LWZlK"})
+
+#print(name.text)
+
+a=0
+for i in name: 
+	
+	print(name[a].text)
+	print(price[a].text)
+	print(rating[a].text)
+
+	print(" \n")
+
+	name1.append(name[a].text)
+	price1.append(price[a].text)
+	rating1.append(rating[a].text)
+
+	a=a+1
+
+
+df = pandas.DataFrame({'NAME':name1,'PRICES':price1,'RATINGS':rating1})
+
+print(df)
